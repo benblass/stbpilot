f7b59ea1eefd6c17747011d6f11c343797845e14
benblass <b.blassel@gmail.com>
2015-07-13 20:38:00 +0200

    Added zone selection behavior and search button action

---
+++ assets/js/stbpilot.js
@@ -0,0 +1,25 @@
+
+function zone_selected(index) {
+	$('#search_button').text('Initiate search on flight zone: ' + flight_zones.features[index].properties.flname);
+	$('#search_button').addClass("activated");
+	$('#search_button').on('click', function () { 
+		initiate_search(index);
+		}
+	);
+	return;
+}
+
+function zone_unselected() {
+	$('#search_button').text("Search");
+	$('#search_button').removeClass("activated");
+	$('#search_button').off();
+	return;
+}
+
+function initiate_search(index) {
+	$('#search_button').addClass('clicked');
+	$('#search_button').removeClass("activated");
+	$('#search_button').off();
+	$("#result").text("Initiating search on Flight Zone " + flight_zones.features[index].properties.flname);
+	return;
+}
